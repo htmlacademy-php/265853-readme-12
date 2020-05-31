cda5303e88e6ab4c427292e580719cf05f47f2b4
EgorTolbaev <egor05.09.97@gmail.com>
2020-05-31 13:18:13 +0500

    Добавил схему БД

---
+++ schema.sql
@@ -0,0 +1,90 @@
+CREATE DATABASE readme
+        DEFAULT CHARACTER SET utf8
+        DEFAULT COLLATE utf8_general_ci;
+
+USE readme;
+
+CREATE TABLE `users` (
+    `id`       INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `date_add` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `email`    VARCHAR(50) NOT NULL UNIQUE,
+    `login`    VARCHAR(50) NOT NULL UNIQUE,
+    `password` VARCHAR(50) NOT NULL,
+    `avatar`   VARCHAR(128)
+);
+
+CREATE TABLE `content_type` (
+    `id`        INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `type_name` VARCHAR(128) NOT NULL UNIQUE,
+    `icon_type` VARCHAR(128) NOT NULL UNIQUE
+);
+
+CREATE TABLE `posts` (
+    `id`       INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `date_add` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `title`    VARCHAR(128),
+    `content_text` TEXT,
+    `quote_author` VARCHAR(128),
+    `img_url`      VARCHAR(128),
+    `video_url`    VARCHAR(128),
+    `link`         VARCHAR(256),
+    `number_views` INT,
+    `user_id` INT NOT NULL,
+    `type_id` INT NOT NULL,
+    FOREIGN KEY (`user_id`)  REFERENCES `users` (`Id`),
+    FOREIGN KEY (`type_id`)  REFERENCES `content_type` (`Id`)
+);
+
+CREATE TABLE `comments` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `date_add` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `content` TEXT,
+    `user_id` INT NOT NULL,
+    `post_id` INT NOT NULL,
+    FOREIGN KEY (`user_id`)  REFERENCES `users` (`Id`),
+    FOREIGN KEY (`post_id`)  REFERENCES `posts` (`Id`)
+);
+
+CREATE TABLE `likes` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `user_id` INT NOT NULL,
+  `post_id` INT NOT NULL,
+  FOREIGN KEY (`user_id`)  REFERENCES `users` (`Id`),
+  FOREIGN KEY (`post_id`)  REFERENCES `posts` (`Id`)
+);
+
+CREATE TABLE `subscriptions` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  `user_id` INT NOT NULL,
+  `subscriber_id` INT NOT NULL,
+  FOREIGN KEY (`user_id`)  REFERENCES `users` (`Id`),
+  FOREIGN KEY (`subscriber_id`)  REFERENCES `users` (`Id`)
+);
+
+CREATE TABLE `messages` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `date_add` TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `content` TEXT,
+    `message_sender` INT NOT NULL,
+    `message_recipient` INT NOT NULL,
+    FOREIGN KEY (`message_sender`)  REFERENCES `users` (`Id`),
+    FOREIGN KEY (`message_recipient`)  REFERENCES `users` (`Id`)
+);
+
+CREATE TABLE `hashtags` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `title` VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE `post_hashtag` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    `post_id` INT NOT NULL,
+    `hashtag_id` INT NOT NULL,
+    FOREIGN KEY (`post_id`)  REFERENCES `posts` (`Id`),
+    FOREIGN KEY (`hashtag_id`)  REFERENCES `hashtags` (`Id`)
+);
+
+CREATE INDEX `c_login`   ON `users`    (`login`);
+CREATE INDEX `c_email`   ON `users`    (`email`);
+CREATE INDEX `c_title`   ON `posts`    (`title`);
+CREATE INDEX `c_hashtag` ON `hashtags` (`title`);
