75e158d29a0b8d2a72a5b4c5d1c6b9cb16534db1
EgorTolbaev <egor05.09.97@gmail.com>
2020-06-07 11:34:10 +0500

    Внес правки в SQL-запросы

--- queries.sql
+++ queries.sql
@@ -5,8 +5,8 @@ INSERT INTO `content_type` (`type_name`,`icon_type`)
 VALUES ('Текст','text'),
 	    ('Цитата','quote'),
 	    ('Картинка','photo'),
-		 ('Видео','video'),
-		 ('Ссылка','link');
+		('Видео','video'),
+		('Ссылка','link');
 
 -- ------------------------------------------------------
 -- Добавление пользователей
@@ -20,7 +20,7 @@ VALUES ('2018-03-25 16:15:00', 'vlad@mail.ru', 'Владик', ' ', 'img/userpic
 -- -----------------------------------------------------
 -- Создание списка постов
 -- -----------------------------------------------------
-INSERT `posts` (`date_add`, `title`,`content_text`,`quote_author`,`img_url`,`video_url`,`link`,`number_views`,`user_id`,`type_id`)
+INSERT INTO `posts` (`date_add`, `title`,`content_text`,`quote_author`,`img_url`,`video_url`,`link`,`number_views`,`user_id`,`type_id`)
 VALUES ('2020-05-31 12:24:12', 'Цитата', 'Мы в жизни любим только раз, а после ищем лишь похожих', 'С.А.Есенин', NULL, NULL, NULL, 10, 3, 2),
        ('2020-05-30 23:43:12', 'Игра престолов',
         'Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала! Не могу дождаться начала финального сезона своего любимого сериала!Не могу дождаться начала финального сезона своего любимого сериала!',
@@ -35,7 +35,7 @@ VALUES ('2020-05-31 12:24:12', 'Цитата', 'Мы в жизни любим т
 -- Создание списка комментариев
 -- -----------------------------------------------------
 
-INSERT `comments` (`date_add`, `content`, `user_id`, `post_id`)
+INSERT INTO `comments` (`date_add`, `content`, `user_id`, `post_id`)
 VALUES ('2020-05-31 12:30:31', 'Скорей бы!!!', 4, 2),
 	    ('2020-04-05 10:10:11', 'Очень красиво', 3, 3);
 
@@ -43,107 +43,106 @@ VALUES ('2020-05-31 12:30:31', 'Скорей бы!!!', 4, 2),
 -- Получить список постов с сортировкой по популярности и вместе с именами авторов и типом контента
 -- -----------------------------------------------------
 
-SELECT P.id, P.title, P.content_text,P.img_url,P.video_url,P.link, P.number_views, US.login, CT.type_name
+SELECT P.`id`, P.`title`, P.`content_text`,P.`img_url`,P.`video_url`,P.`link`, P.`number_views`, US.`login`, CT.`type_name`
 FROM 	`posts` AS P
 	INNER JOIN  `users` AS US
-		ON P.user_id = US.id
+		ON P.`user_id` = US.`id`
 	INNER JOIN  `content_type` AS CT
-		ON P.type_id = CT.id
-ORDER BY  P.number_views DESC
+		ON P.`type_id` = CT.`id`
+ORDER BY  P.`number_views` DESC
 
 #Можно еще так, но при большом количестве данных нужно посмотреть на производительность
 SELECT `ALL`.* FROM (
-	SELECT  P.id, P.title,P.content_text, P.number_views, US.login, CT.type_name
+	SELECT  P.`id`, P.`title`,P.`content_text`, P.`number_views`, US.`login`, CT.`type_name`
 		FROM 	`posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
+				ON P.`user_id` = US.`id`
 			INNER JOIN  `content_type` AS CT
-				ON P.type_id = CT.id
-		WHERE P.content_text IS  NOT null
+				ON P.`type_id` = CT.`id`
+		WHERE P.`content_text` IS  NOT null
 UNION
-	SELECT  P.id, P.title,P.img_url, P.number_views, US.login, CT.type_name
+	SELECT  P.`id`, P.`title`,P.`img_url`, P.`number_views`, US.`login`, CT.`type_name`
 		FROM 	`posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
+				ON P.`user_id` = US.`id`
 			INNER JOIN  `content_type` AS CT
-				ON P.type_id = CT.id
-		WHERE P.img_url IS  NOT null
+				ON P.`type_id` = CT.`id`
+		WHERE P.`img_url` IS  NOT null
 UNION
-	SELECT  P.id, P.title,P.video_url, P.number_views, US.login, CT.type_name
+	SELECT  P.`id`, P.`title`,P.`video_url`, P.`number_views`, US.`login`, CT.`type_name`
 		FROM 	`posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
+				ON P.`user_id` = US.`id`
 			INNER JOIN  `content_type` AS CT
-				ON P.type_id = CT.id
-		WHERE P.video_url IS  NOT null
+				ON P.`type_id` = CT.`id`
+		WHERE P.`video_url` IS  NOT null
 UNION
-	SELECT  P.id, P.title,P.link, P.number_views, US.login, CT.type_name
+	SELECT  P.`id`, P.`title`,P.`link`, P.`number_views`, US.`login`, CT.`type_name`
 		FROM 	`posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
+				ON P.`user_id` = US.`id`
 			INNER JOIN  `content_type` AS CT
-				ON P.type_id = CT.id
-		WHERE P.link IS  NOT null
+				ON P.`type_id` = CT.`id`
+		WHERE P.`link` IS  NOT null
 ) AS `ALL`
-ORDER BY  `ALL`.number_views DESC
+ORDER BY  `ALL`.`number_views` DESC
 
 -- -----------------------------------------------------
 -- Получить список постов для конкретного пользователя
 -- -----------------------------------------------------
 
-SELECT P.id, P.content_text,P.img_url,P.video_url,P.link, US.login
+SELECT P.`id`, P.`content_text`,P.`img_url`,P.`video_url`,P.`link`,US.`login`
 FROM `posts` AS P
 	INNER JOIN  `users` AS US
-		ON P.user_id = US.id
-WHERE US.id = 1
+		ON P.`user_id` = US.`id`
+WHERE US.`id` = 1
 
 #Можно еще так, но при большом количестве данных нужно посмотреть на производительность
-SELECT id, content_text, login
+SELECT `id`, `content_text`, `login`
 FROM (
-	SELECT P.id, P.content_text, US.login,US.id AS uID
+	SELECT P.`id`, P.`content_text`, US.`login`,US.`id` AS uID
 		FROM `posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
-		WHERE P.content_text IS NOT NULL
+				ON P.`user_id` = US.`id`
+		WHERE P.`content_text` IS NOT NULL
 UNION
-	SELECT P.id, P.img_url, US.login,US.id AS uID
+	SELECT P.`id`, P.`img_url`, US.`login`,US.`id` AS uID
 		FROM `posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
-		WHERE P.img_url IS NOT NULL
+				ON P.`user_id` = US.`id`
+		WHERE P.`img_url` IS NOT NULL
 UNION
-	SELECT P.id, P.video_url, US.login,US.id AS uID
+	SELECT P.`id`, P.`video_url`, US.`login`,US.`id` AS uID
 		FROM `posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
-		WHERE P.video_url IS NOT NULL
+				ON P.`user_id` = US.`id`
+		WHERE P.`video_url` IS NOT NULL
 UNION
-	SELECT P.id, P.link, US.login,US.id AS uID
+	SELECT P.`id`, P.`link`, US.`login`,US.`id` AS uID
 		FROM `posts` AS P
 			INNER JOIN  `users` AS US
-				ON P.user_id = US.id
-		WHERE P.link IS NOT NULL
+				ON P.`user_id` = US.`id`
+		WHERE P.`link` IS NOT NULL
 )`ALL`
-WHERE `ALL`.uID = 1
+WHERE `ALL`.`uID` = 1
 
 -- -----------------------------------------------------------------------------------------------------
 -- Получить список комментариев для одного поста, в комментариях должен быть логин пользователя
 -- -----------------------------------------------------------------------------------------------------
 
-SELECT P.id, C.content, US.login
+SELECT P.`id`, C.`content`, US.`login`
 FROM `comments` AS C
 	INNER JOIN  `users` AS US
-		ON C.user_id = US.id
+		ON C.`user_id` = US.`id`
 	INNER JOIN `posts` AS P
-		ON C.post_id = P.id
-WHERE P.id = 2
-
+		ON C.`post_id` = P.`id`
+WHERE P.`id` = 2
 
 -- -----------------------------------------------------
 -- Добавить лайк к посту
 -- -----------------------------------------------------
 
-INSERT `likes` (`user_id`, `post_id`)
+INSERT INTO `likes` (`user_id`, `post_id`)
 VALUES
        (1, 3),
        (2, 5),
@@ -153,5 +152,5 @@ VALUES
 -- Подписаться на пользователя
 -- -----------------------------------------------------
 
-INSERT `subscriptions` (`user_id`, `subscriber_id`)
+INSERT  INTO `subscriptions` (`user_id`, `subscriber_id`)
 VALUES (4, 1);
